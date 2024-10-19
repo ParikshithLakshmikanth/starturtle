b5d25a9ddcba15ad82232a0370b2ac12f6376dbe
ParikshithLakshmikanth <132085131+ParikshithLakshmikanth@users.noreply.github.com>
2024-10-19 17:43:26 +0530

    star

---
+++ main.py
@@ -0,0 +1,27 @@
+import turtle
+
+turtle.Screen().bgcolor("Orange")
+
+sc = turtle.Screen()
+sc.setup(1280,720)
+
+board = turtle.Turtle()
+
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.left(120)
+board.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(50)
+
+board.pendown()
+board.right(90)
+
+board.forward(100)
+board.right(120)
+board.forward(100)
+board.right(120)
+board.forward(100)
